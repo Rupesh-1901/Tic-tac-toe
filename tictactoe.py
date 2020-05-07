7740e7e51282ba3f9767b1246d93047c8952eec3
Rupesh-1901 <64422429+Rupesh-1901@users.noreply.github.com>
2020-05-07 21:02:08 +0530

    Create tictactoe.py

---
+++ tictactoe.py
@@ -0,0 +1,110 @@
+def display_board(board):
+    print(board[7] + ' || '+ board[8] + ' || ' + board[9])
+    print(board[4] + ' || '+ board[5] + ' || ' + board[6])
+    print(board[1] + ' || '+ board[2] + ' || ' + board[3])
+test_board=['']*10
+display_board(test_board)
+def player_output():
+    '''
+    OUTPUT = (Player1:choose X or O)
+    '''
+    marker = ''
+    while marker != 'X' and marker != 'O' :
+        marker = input('player1:choose x or o: ')
+
+    if marker == 'X' :
+        return ('X','O')
+    else:
+        return('O','X')
+#player_output()
+def place_marker(board,marker,position):                    
+    board[position] = marker
+def win_check(board,mark):
+    #all rows share  same mark
+    return(  (board[7] == board[8] == board [9] == mark)or
+             (board[4] == board[5] == board [6] == mark)or
+             (board[3] == board[2] == board [1] == mark)or
+    #all columns share same mark
+             (board[7] == board[4] == board [1] == mark)or
+             (board[8] == board[5] == board [2] == mark)or
+             (board[9] == board[6] == board [3] == mark)or
+    #all diagonals share same mark
+             (board[7] == board[5] == board [3] == mark)or
+             (board[1] == board[5] == board [9] == mark))
+import random
+def choose_first():
+    flip = random.randint(0,1)
+    if flip == 0:
+        return 'player 1'
+    else:
+        return 'player 2'
+def space_check(board,position):
+    return board[position] == ''
+def full_board_check(board):
+    for i in range(1,10):
+         if space_check(board,i):
+             return False
+    return True
+def player_choice(board):
+    position = 0
+    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board,position):
+        position = int(input('choose a position: (1-9)'))
+
+        return position
+def replay():
+    choice = input("play again ? enter yes or nooooooo")
+    return choice == 'yes'
+print('welcome to the game')
+while True:
+    the_board = ['']*10
+    player1_marker, player2_marker=  player_output()
+    turn=choose_first()
+    print(turn+'will go first')
+    play_game=input('ready to play?? yup or nope')
+    if play_game=='yup':
+        game_on= True
+    else:
+        game_on= False
+    while game_on:
+        if turn=='player 1':
+            display_board(the_board)
+            position=player_choice(the_board)
+            place_marker(the_board,player1_marker,position)
+            if win_check(the_board,player1_marker):
+                display_board(the_board)
+                print('player 1 has won')
+                game_on = False
+            else:
+                if full_board_check(the_board):
+                    display_board(the_board)
+                    print('tie game')
+                    game_on=False
+                else:
+                    turn='player 2'
+        else:
+            display_board(the_board)
+            position=player_choice(the_board)
+            place_marker(the_board,player2_marker,position)
+            if win_check(the_board,player2_marker):
+                display_board(the_board)
+                print('player 2 has won')
+                game_on= False
+            else:
+                if full_board_check(the_board):
+                    display_board(the_board)
+                    print('tie game')
+                    game_on= False
+                else:
+                    turn='player 1'
+
+    if not replay():
+        break
+        
+    
+    
+
+ 
+
+
+            
+
